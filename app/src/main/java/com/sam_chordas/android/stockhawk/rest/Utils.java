d01a1d5b531e17a9e03195d416bcf734b7b31145
Harry Grillo <harry@redbassett.com>
2016-06-28 13:12:16 -0400

    Add name column to database

--- app/src/main/java/com/sam_chordas/android/stockhawk/rest/Utils.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/rest/Utils.java
@@ -2,12 +2,15 @@ package com.sam_chordas.android.stockhawk.rest;
 
 import android.content.ContentProviderOperation;
 import android.util.Log;
+
 import com.sam_chordas.android.stockhawk.data.QuoteColumns;
 import com.sam_chordas.android.stockhawk.data.QuoteProvider;
-import java.util.ArrayList;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * Created by sam_chordas on 10/8/15.
@@ -75,6 +78,7 @@ public class Utils {
         QuoteProvider.Quotes.CONTENT_URI);
     try {
       String change = jsonObject.getString("Change");
+      builder.withValue(QuoteColumns.NAME, jsonObject.getString("Name"));
       builder.withValue(QuoteColumns.SYMBOL, jsonObject.getString("symbol"));
       builder.withValue(QuoteColumns.BIDPRICE, truncateBidPrice(jsonObject.getString("Bid")));
       builder.withValue(QuoteColumns.PERCENT_CHANGE, truncateChange(
